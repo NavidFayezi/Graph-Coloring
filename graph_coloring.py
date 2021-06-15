2bc3eb442761d149af76e2a4bc5b91084f7934ba
NavidFayezi <navidfayezi.98@gmail.com>
2021-06-15 11:56:17 +0430

    add degree to Vertex class

--- graph_coloring.py
+++ graph_coloring.py
@@ -11,22 +11,27 @@ class Vertex:
     def __init__(self, v: int):
         self.vertex_number = v
         self.adjacencies = []
+        self.degree = 0
 
     def add_adjacency(self, vertex: '__main__.Vertex'):
-        self.adjacencies.append(vertex)
+        if vertex not in self.adjacencies:
+            self.adjacencies.append(vertex)
+            self.degree += 1
 
 
 class Graph:
     def __init__(self):
         self.vertices = []
         self.edges = []
-        self.delta = 0
+        self.min_degree = 0
 
 
 def get_input():
     input_line = input()
-    input_line.split(" ")
-    print(input_line)
+    input_line = input_line.split(" ")
+    assert len(input_line) == 2
+    number_of_vertices = int(input_line[0])
+    number_of_edges = int(input_line[1])
 
 
-
+get_input()
